be3710426bed6598bc89d05df25f2c487c93105c
brenimsilva <brenoalmeida.dev@gmail.com>
2023-10-08 22:11:28 -0300

    [Auth]: routes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +11,16 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   userForm: FormGroup
 
-  constructor(fb: FormBuilder, private service: AuthService, private router: Router) {
+  constructor(fb: FormBuilder, private service: AuthService, private aRouter: ActivatedRoute, private router: Router) {
     this.userForm = fb.group({UserEmail: "", UserPassword: ""});
   }
   login() {
     const formData = this.userForm.value;
     this.service.userLogIn(formData.UserEmail, formData.UserPassword).subscribe((res) => {
-      this.router.navigateByUrl("/success");
+      this.aRouter.queryParams.subscribe((params) => {
+        console.log(params['continue']);
+          window.location.href= `${params['continue']}?token=${res.userToken}`;
+      });
     });
   }
 
